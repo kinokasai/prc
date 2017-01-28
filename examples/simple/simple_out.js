d3b04c5a4d038b4029d1581b15a17f6253f3f672
Alexandre Doussot <doussot.alexandre@gmail.com>
2017-01-28 15:48:06 +0000

    Remove reset functions

--- examples/simple/simple_out.js
+++ examples/simple/simple_out.js
@@ -20,46 +20,25 @@ var dir_enum = Object.freeze({
 });
 
 function move() {
-    this.r = true;
     this.x = 0;
     this.y = 0;
-    this.prev_x = 0;
-    this.prev_y = 0;
-}
-
-move.prototype.reset = function () {
-    this.r = true;
-    this.x = 0;
-    this.y = 0;
-    this.prev_x = 0;
-    this.prev_y = 0;
 }
 
 move.prototype.step = function (ck, key) {
-    console.log("x: " + this.x);
-    console.log("y: " + this.y);
-    console.log("reset: " + this.r);
-    var x_1 = 0;
-    var y_1 = 0;
-    var reset = this.r;
-    this.r = false;
+    //console.log("x: " + this.x);
+    //console.log("y: " + this.y);
+    //console.log("reset: " + this.r);
     var dir = dir_node.step(key);
-    if (reset) {x_1 = this.x;} else {x_1 = move_x_node.step(dir, this.prev_x);};
-    if (reset) {y_1 = this.y;} else {y_1 = move_y_node.step(dir, this.prev_y);};
-    this.prev_x = x_1;
-    this.prev_y = y_1;
+    var x_1 = move_x_node.step(dir, this.x);
+    var y_1 = move_y_node.step(dir, this.y);
+    this.x = x_1;
+    this.y = y_1;
     var point = {x: x_1, y: y_1};
     return point;
 }
 
 function move_x() {
-    this.r = true;
-    this.new_x = undefined;
-}
-
-move_x.prototype.reset = function () {
-    this.r = true;
-    this.new_x = undefined;
+    /* No internal state */
 }
 
 move_x.prototype.step = function (dir, x) {
@@ -79,21 +58,13 @@ move_x.prototype.step = function (dir, x) {
             console.log(dir);
     }
     new_x = x_1;
-    console.log("new_x: " + new_x);
     return new_x;
 }
 
 function move_y() {
-    this.r = true;
-}
-
-move_y.prototype.reset = function () {
-    this.r = true;
 }
 
 move_y.prototype.step = function (dir, y) {
-    reset = this.r;
-    this.r = false;
     switch (dir) {
         case dir_enum.Left:
         case dir_enum.Right:
@@ -112,11 +83,6 @@ move_y.prototype.step = function (dir, y) {
 }
 
 function dir() {
-    this.r = true;
-}
-
-dir.prototype.reset = function () {
-    this.r = true;
 }
 
 dir.prototype.step = function (key) {
@@ -133,16 +99,9 @@ dir.prototype.step = function (key) {
 }
 
 function key_press() {
-    this.r = true;
-}
-
-key_press.prototype.reset = function () {
-    this.r = true;
 }
 
 key_press.prototype.step = function (key, n) {
-    reset = this.r;
-    this.r = false;
     var v = (key == KD(n));
     var b_1 = key != KU(n);
     if (b_1) {pressed = v;} else {pressed = false;}
@@ -157,11 +116,6 @@ var move_y_node = new move_y();
 var dir_node = new dir();
 
 function init_nodes() {
-    key_press_node.reset();
-    move_x_node.reset();
-    move_y_node.reset();
-    move_node.reset();
-    dir_node.reset();
     move_node_ret = move_node.step(true, -1);
 }
 
