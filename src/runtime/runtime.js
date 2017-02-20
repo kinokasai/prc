e3242830379a81d2557a4506e6b2220e8180b549
Alexandre Doussot <doussot.alexandre@gmail.com>
2017-02-20 14:23:52 +0000

    Fix runtime

--- src/runtime/runtime.js
+++ src/runtime/runtime.js
@@ -1,2 +1,10 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
+
+function draw_rect(x, y, w, h) {
+    ctx.beginPath();
+    ctx.rect(x, y, 20, 20);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
