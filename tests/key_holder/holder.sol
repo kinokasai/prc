cdb9b5edc0795c0cd1e0a59c847940a71b5bb44e
Alexandre Doussot <doussot.alexandre@gmail.com>
2017-02-20 14:21:14 +0000

    Fix key_holder to use new grammar.

--- tests/key_holder/holder.sol
+++ tests/key_holder/holder.sol
@@ -1,14 +1,15 @@
 type key = KU | KD
+type bool = True | False
 
 machine main =
     memory k : key
     instances is_down : is_down
     reset () =
         state(k) = KU
-    step (ck : bool, key : key :: ck) returns (b : bool)
+    step (ck : bool, key : key) returns (b : bool) =
     var b : bool in
     case (ck) {
-        True: state(key) = key;
+        True: state(key) = key
     };
     b = is_down.step(state(key))
 
@@ -19,6 +20,6 @@ machine is_down =
     step (key : key) returns (b : bool) =
         var b : bool in
     case (key) {
-        KD: b = True;
-        KU: b = False;
+        KD: b = True |
+        KU: b = False
     }
