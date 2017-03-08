66713bfd819ea02c29378871e19a5160143903ea
Alexandre Doussot <doussot.alexandre@gmail.com>
2017-03-08 12:00:51 +0000

    Travis

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -5,6 +5,9 @@ echo "yes" | sudo add-apt-repository ppa:$ppa
 sudo apt-get update -qq
 sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
 export OPAMYES=1
+opam update
+opam upgrade
+opam switch $OPAM_SWITCH
 opam init
 opam install ${OPAM_DEPENDS}
 eval `opam config env`
